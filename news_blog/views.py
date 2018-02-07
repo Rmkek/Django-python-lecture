9725a874af87fab0e88cd7f2a5d2a53873bbedb3
Malyshkov Roman <malyshkov.roman@gmail.com>
2018-02-07 17:37:20 +0600

    Lecture #3 task.

--- news_blog/views.py
+++ news_blog/views.py
@@ -1,27 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-ARTICLES = [
-    {
-        'id': 1,
-        'title': 'First news',
-        'text': 'This is the worst first article'
-    },
-    {
-        'id': 2,
-        'title': 'Second news',
-        'text': 'This is the amazing second article'
-    }]
+from django.http import HttpResponseNotFound
+from .models import Article, Author, Comment
 
 
 def index(request):
-    return render(request, 'news_blog/news_list.html', {'articles': ARTICLES})
+    articles = Article.objects.all().order_by('published_date')
+
+    return render(request, 'news_blog/news_list.html', {'articles': articles})
 
 
 def get_article(request, article_id):
-    article = ARTICLES[article_id - 1]
-    return render(request, 'news_blog/news_article.html', {'article': article})
+    try:
+        article = Article.objects.get(id=article_id)
+        author = Author.objects.filter(article__pk=article_id)
+        comment = Comment.objects.filter(article=article)
+        print(comment)
+
+    except (Article.DoesNotExist, Author.DoesNotExist):
+        return HttpResponseNotFound()
+
+    return render(request, 'news_blog/news_article.html', {'article': article, 'author': author[0], 'comment': comment[0]})
 
 
 def get_about_me(request):
